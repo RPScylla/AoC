6c526e24d57b1bc399ed563508cc45990962f820
RPScylla <info@rpsonline.nl>
2018-12-16 23:32:00 +0100

    + Day14

--- AoC2018/Days/Day14.cs
+++ AoC2018/Days/Day14.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace RPSonline.AoC.E2018.Days
 {
@@ -13,18 +14,73 @@ namespace RPSonline.AoC.E2018.Days
     {
         private const int AOC_DAY = 14;
 
-        private readonly string[] _input;
+        private readonly int _input;
+        private readonly string _inputstr;
 
         public Day14()
         {
-            _input = File.ReadAllLines(FileHelper.GetInputLocation(AOC_DAY, Consts.AOC_YEAR));
+            _inputstr = File.ReadAllText(FileHelper.GetInputLocation(AOC_DAY, Consts.AOC_YEAR));
+            _input = int.Parse(_inputstr);
         }
 
         public AnswerModel Solve()
         {
             AnswerModel answer = new AnswerModel();
 
+            List<int> ElvesIndex = new List<int>();
+            for (int i = 0; i < 2; i++)
+            {
+                ElvesIndex.Add(i);
+            }
+
+            StringBuilder recepies = new StringBuilder("37", 30000000);
+            int saveLocation = 0;
+            while (answer.PartB is null || recepies.Length <_input + 10)
+            {
+                int sum = 0;
+                for (int j = 0; j < ElvesIndex.Count; j++)
+                {
+                    int current = recepies[ElvesIndex[j]] - 48;
+                    sum += current;
+                }
+
+                recepies.Append(sum);
+
+                for (int j = 0; j < ElvesIndex.Count; j++)
+                {
+                    int current = recepies[ElvesIndex[j]] - 48;
+                    ElvesIndex[j] = (ElvesIndex[j] + current + 1) % recepies.Length;
+                }
+
+                while (answer.PartB is null && recepies.Length > saveLocation + _inputstr.Length)
+                {
+                    bool found = true;
+
+                    for (int i = 0; i < _inputstr.Length; i++)
+                    {
+                        if(recepies[i + saveLocation] != _inputstr[i])
+                        {
+                            found = false;
+                            break;
+                        }
+                    }
+
+                    if(found)
+                    {
+                        answer.PartB = saveLocation;
+                    }
+
+                    saveLocation++;
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                answer.PartA += recepies[i+_input].ToString();
+            }
+
             return answer;
         }
+
     }
 }
